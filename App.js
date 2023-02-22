899ca1bfc7abf8b8aeb72ebb6beba2c95c651873
Aleksanrda <kryvkoalexandra05@gmail.com>
2023-02-22 16:15:05 +0000

    implement favourites page

--- App.js
+++ App.js
@@ -6,18 +6,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import SwipePage from "./components/SwipePage";
 import FavouritesPage from "./components/FavouritesPage";
+import BasketPage from "./components/BasketPage";
 
 export default function App() {
 	const Tab = createMaterialTopTabNavigator();
 	const [fav, setFav] = useState([]);
+	const [basket, setBasket] = useState([]);
 
 	return (
 		<NavigationContainer>
 			<Tab.Navigator style={styles.tab}>
      			 <Tab.Screen name="Home" component={SwipePage} />
-     			 <Tab.Screen name="Favourites" component={FavouritesPage} />
+     			 <Tab.Screen name="Favourites" children={(props) => <FavouritesPage setBasket={setBasket} />} />
+				 <Tab.Screen name="Basket" component={BasketPage} basket={basket} />
     		</Tab.Navigator>
-			
+
 		</NavigationContainer>
 	);
 }
--- components/FavouritesPage.jsx
+++ components/FavouritesPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   View,
@@ -13,98 +13,119 @@ import {
 const DATA = [
   {
     item_id: 0,
-    title: "Blue Denim Jacket",
-    price: 50,
-    color: "blue",
-    category: "trousers",
-    style: "casual",
-    material: "denim",
-    item_img_url: "https://i.imgur.com/OYkyR5m.jpg",
+    title: "Brown Bomber Jacket",
+    price: 45,
+    color: "Brown",
+    category: "activewear",
+    brand: "Columbia",
+    gender: "male",
+    item_img_url: "https://i.imgur.com/cuO6Oro.jpg",
   },
   {
     item_id: 1,
     title: "Cat Print Shirt",
     price: 20,
-    color: "white",
-    category: "shirts",
-    style: "casual",
-    material: "cotton",
+    color: "CREAM",
+    category: "activewear",
+    brand: "Vans",
+    gender: "male",
     item_img_url: "https://i.imgur.com/rVbTPTD.jpg",
   },
   {
     item_id: 2,
     title: "Trendy Red Shoes",
     price: 60,
-    color: "red",
-    category: "shoes",
-    style: "casual",
-    material: "leather",
+    color: "WHITE",
+    category: "activewear",
+    brand: "Vans",
+    gender: "male",
     item_img_url: "https://i.imgur.com/ksRACaQ.jpg",
   },
   {
     item_id: 3,
     title: "Grey Ripped Jeans",
     price: 35,
-    color: "grey",
-    category: "trousers",
-    style: "casual",
-    material: "denim",
+    color: "Multi",
+    category: "activewear",
+    brand: "New Balance",
+    gender: "male",
     item_img_url: "https://i.imgur.com/IXovKq2.jpg",
   },
   {
     item_id: 4,
     title: "Brown Bomber Jacket",
     price: 45,
-    color: "brown",
-    category: "jackets",
-    style: "casual",
-    material: "cotton",
+    color: "WHITE",
+    category: "activewear",
+    brand: "Reebok",
+    gender: "male",
     item_img_url: "https://i.imgur.com/cuO6Oro.jpg",
   },
   {
     item_id: 5,
     title: "Red Running Shoes",
     price: 45,
-    color: "red",
-    category: "shoes",
-    style: "athletic",
-    material: "polyester",
+    color: "Black",
+    category: "activewear",
+    brand: "Quiksilver",
+    gender: "male",
     item_img_url: "https://i.imgur.com/GLsHdA5.jpg",
   },
 ];
 
-const handleAddToBasketButton = () => {
-  console.log("press on button => now do smth");
-}
+const FavouritesPage = ({ navigation, setBasket }) => {
+  const handleNavigateToBasketPage = () => {
+    navigation.navigate('Basket');
+  };
 
-const Item = ({item}) => (
-  <View style={styles.item}>
-  <View style={styles.top}>
-  <View>
-    <Text style={styles.title}>{item.title}</Text>
-    <Text style={styles.title}>material: {item.material}</Text>
-  </View>
-    <Image style={styles.productImage} source={{
-						uri: item.item_img_url
-					}}></Image>
-  </View>
-  <View style={styles.bottom}>
-    <Text style={styles.title}>price: £{item.price}</Text>
-    <Pressable style={styles.button} onPress={handleAddToBasketButton}>
-    <Text style={styles.text}>Add to basket</Text>
-    </Pressable>
-    </View>
-  </View>
-);
+  const handleAddToBasketButton = (item) => {
+    console.log("See current busket!");
+    console.log(item);
+    if (item.title) {
+      setBasket(currentBasket => [item, ...currentBasket]);
+    }
+  };
 
-const FavouritesPage = () => {
+  const renderItem = ({item}) => {
+    return (
+        <View style={styles.item}>
+          <View style={styles.left}>
+            <View style={styles.description}>
+              <Text style={styles.title}>{item.title}</Text>
+              <View style={styles.category}>
+                <Text style={styles.categoryTitle}>category: </Text>
+                <Text style={styles.categoryValue}>{item.category}</Text>
+              </View>
+            </View>
+            <View style={styles.price}>
+              <Text style={styles.priceTitle}>price: </Text>
+              <Text style={styles.priceValue}>£{item.price}</Text>
+            </View>
+          </View>
+          <View style={styles.right}>
+            <Image style={styles.productImage} source={{
+                      uri: item.item_img_url
+                    }} />
+            <Pressable style={styles.button} onPress={() => handleAddToBasketButton(item)}>
+              <Text style={styles.text}>Add to basket</Text>
+            </Pressable>
+          </View>
+        </View>
+    );
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={DATA}
-        renderItem={({item}) => <Item item={item} />}
+        renderItem={renderItem}
         keyExtractor={item =>item.item_id}
       />
+      <View>
+        <Pressable style={styles.viewBasketButton} onPress={handleNavigateToBasketPage}>
+          <Text style={styles.viewBasketTitle}>View Basket</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
@@ -113,33 +134,55 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    justifyContent: 'center',
   },
   item: {
-    backgroundColor: '#f0ffff',
+    backgroundColor: '#F6F3F1',
+    borderWidth: 3,
+    borderRadius: 5,
+    borderColor: '#606476',
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 8,
+    flexDirection: 'row',
   },
   title: {
-    fontSize: 20,
-    paddingRight: 10,
+    fontSize: 25,
+    paddingRight: 5,
+    fontWeight: 'bold',
+    color: '#606476',
   },
   productImage: {
-		width: 150,
-		height: 150,
+		width: 130,
+		height: 130,
 		marginBottom: 10,
-		borderRadius: 20,
+		borderRadius: 10,
 	},
-  button: {
-    height: 60,
+  viewBasketButton: {
+    height: 45,
+    width: 250,
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
     borderRadius: 8,
     elevation: 3,
-    backgroundColor: '#ff8c00',
-
+    backgroundColor: '#EE6E29',
+    marginLeft: 70,
+  },
+  viewBasketTitle: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+    padding: 12,
+  },
+  button: {
+    height: 65,
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    elevation: 6,
+    backgroundColor: '#008C83',
   },
   text: {
       fontSize: 16,
@@ -148,15 +191,42 @@ const styles = StyleSheet.create({
       letterSpacing: 0.25,
       color: 'white',
   },
-  top: {
-    flex: 2,
-    flexDirection: 'row',
+  left: {
+    flex: 3,
     justifyContent: 'space-around',
   },
-  bottom: {
+  right: {
     flex: 2,
+    justifyContent: 'center',
+  },
+  description: {
+    paddingBottom: 10,
+  },
+  price: {
     flexDirection: 'row',
-  }
+  },
+  priceTitle: {
+    fontSize: 20,
+    color: '#606476',
+  },
+  priceValue: {
+    color: '#B8354E',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  category: {
+    marginTop: 15,
+    flexDirection: 'row',
+  },
+  categoryTitle: {
+    color: '#606476',
+    fontSize: 20,
+  },
+  categoryValue: {
+    color: '#B4A6AB',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
 });
 
 export default FavouritesPage;
