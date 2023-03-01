e1f24729c08f738dacf8a77c47ad04929f583609
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-03-01 11:44:11 +0000

    added developer settings for tags

--- components/EditTagFrequency.jsx
+++ components/EditTagFrequency.jsx
@@ -1,117 +1,163 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import { View, Text, TextInput, Button } from "react-native";
-// import { getUser, patchUserPreferences } from "../utils/api";
-// import { UserContext } from "../contexts/userContext";
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import { Button } from "@react-native-material/core";
+import { getUser, patchUserPreferences } from "../utils/api";
+import { UserContext } from "../contexts/userContext";
 
-// const EditTagFrequency = () => {
-//   const [topAndRandom, setTopAndRandom] = useState({});
-//   const { user } = useContext(UserContext);
-//   const [preferences, setPreferences] = useState({});
+const DeveloperSettings = () => {
+  const [showEditTagFrequency, setShowEditTagFrequency] = useState(false);
 
-//   useEffect(() => {
-//     // get user's preferences
-//     const fetchUserDataThenSetPreferences = async () => {
-//       try {
-//         const userFromAPI = await getUser(user);
-//         const existingUserPreferences = JSON.parse(
-//           userFromAPI.data.user.preferences
-//         );
-//         setPreferences(existingUserPreferences);
-//         setTopAndRandom(existingUserPreferences.topAndRandom);
-//       } catch (err) {
-//         console.log(err, "couldnt fetch existing user preferenceser");
-//       }
-//     };
+  return (
+    <View style={styles.margin}>
+      <Button
+        title="Developer Settings"
+        onPress={() => setShowEditTagFrequency(!showEditTagFrequency)}
+      />
+      {showEditTagFrequency && <EditTagFrequency />}
+    </View>
+  );
+};
 
-//     fetchUserDataThenSetPreferences();
-//   }, []);
+const EditTagFrequency = () => {
+  const [topAndRandom, setTopAndRandom] = useState({});
+  const { user } = useContext(UserContext);
+  const [preferences, setPreferences] = useState({});
 
-//   const handleTopAndRandomChange = (key, type, value) => {
-//     if (value >= 0) {
-//       setTopAndRandom({
-//         ...topAndRandom,
-//         [key]: {
-//           ...topAndRandom[key],
-//           [type]: value,
-//         },
-//       });
-//     }
-//   };
+  useEffect(() => {
+    // get user's preferences
+    const fetchUserDataThenSetPreferences = async () => {
+      try {
+        const userFromAPI = await getUser(user);
+        const existingUserPreferences = JSON.parse(
+          userFromAPI.data.user.preferences
+        );
+        setPreferences(existingUserPreferences);
+        setTopAndRandom(existingUserPreferences.topAndRandom);
+      } catch (err) {
+        console.log(err, "couldnt fetch existing user preferenceser");
+      }
+    };
 
-//   const handleSubmit = async () => {
-//     const updatedPreferences = {
-//       ...preferences,
-//       topAndRandom,
-//     };
-//     const updatedPreferencesString = JSON.stringify(updatedPreferences);
-//     try {
-//       await patchUserPreferences(user, {
-//         preferences: updatedPreferencesString,
-//       });
-//       console.log("preferences updated success:");
-//     } catch (err) {
-//       console.error("error updating preferences:", err);
-//     }
-//   };
+    fetchUserDataThenSetPreferences();
+  }, []);
 
-//   return (
-//     <View>
-//       {Object.keys(topAndRandom).map((key) => (
-//         <View key={key}>
-//           <View
-//             style={{
-//               flexDirection: "row",
-//               justifyContent: "center",
-//               alignItems: "center",
-//             }}
-//           >
-//             <Text>{key} top:</Text>
-//             <Button
-//               title="-"
-//               onPress={() =>
-//                 handleTopAndRandomChange(key, "n", +topAndRandom[key].n - 1)
-//               }
-//             />
-//             <TextInput
-//               value={topAndRandom[key].n.toString()}
-//               onChangeText={(text) =>
-//                 handleTopAndRandomChange(key, "n", Number(text))
-//               }
-//             />
-//             <Button
-//               title="+"
-//               onPress={() =>
-//                 handleTopAndRandomChange(key, "n", +topAndRandom[key].n + 1)
-//               }
-//             />
-//           </View>
-//           <View style={{ flexDirection: "row" }}>
-//             <Text>{key} random:</Text>
-//             <Button
-//               title="-"
-//               onPress={() =>
-//                 handleTopAndRandomChange(key, "r", +topAndRandom[key].r - 1)
-//               }
-//             />
-//             <TextInput
-//               value={topAndRandom[key].r.toString()}
-//               onChangeText={(text) =>
-//                 handleTopAndRandomChange(key, "r", Number(text))
-//               }
-//             />
-//             <Button
-//               title="+"
-//               onPress={() =>
-//                 handleTopAndRandomChange(key, "r", +topAndRandom[key].r + 1)
-//               }
-//             />
-//           </View>
-//         </View>
-//       ))}
+  const handleTopAndRandomChange = (key, type, value) => {
+    if (value >= 0) {
+      setTopAndRandom({
+        ...topAndRandom,
+        [key]: {
+          ...topAndRandom[key],
+          [type]: value,
+        },
+      });
+    }
+  };
 
-//       <Button title="Submit" onPress={handleSubmit} />
-//     </View>
-//   );
-// };
+  const handleSubmit = async () => {
+    const updatedPreferences = {
+      ...preferences,
+      topAndRandom,
+    };
+    const updatedPreferencesString = JSON.stringify(updatedPreferences);
+    try {
+      await patchUserPreferences(user, {
+        preferences: updatedPreferencesString,
+      });
+      console.log("preferences updated success:");
+    } catch (err) {
+      console.error("error updating preferences:", err);
+    }
+  };
 
-// export default EditTagFrequency;
+  return (
+    <View>
+      {Object.keys(topAndRandom).map((key) => (
+        <View key={key}>
+          <View style={styles.rows}>
+            <Text>{key} top:</Text>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="-"
+                style={styles.button}
+                onPress={() =>
+                  handleTopAndRandomChange(key, "n", +topAndRandom[key].n - 1)
+                }
+              />
+              <TextInput
+                value={" " + topAndRandom[key].n.toString() + " "}
+                onChangeText={(text) =>
+                  handleTopAndRandomChange(key, "n", Number(text))
+                }
+              />
+              <Button
+                title="+"
+                style={styles.button}
+                onPress={() =>
+                  handleTopAndRandomChange(key, "n", +topAndRandom[key].n + 1)
+                }
+              />
+            </View>
+          </View>
+          <View style={styles.rows}>
+            <Text>{key} random:</Text>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="-"
+                style={styles.button}
+                onPress={() =>
+                  handleTopAndRandomChange(key, "r", +topAndRandom[key].r - 1)
+                }
+              />
+              <TextInput
+                value={" " + topAndRandom[key].r.toString() + " "}
+                onChangeText={(text) =>
+                  handleTopAndRandomChange(key, "r", Number(text))
+                }
+              />
+              <Button
+                title="+"
+                style={styles.button}
+                onPress={() =>
+                  handleTopAndRandomChange(key, "r", +topAndRandom[key].r + 1)
+                }
+              />
+            </View>
+          </View>
+        </View>
+      ))}
+
+      <Button title="Submit" onPress={handleSubmit} />
+    </View>
+  );
+};
+
+export default DeveloperSettings;
+
+const styles = StyleSheet.create({
+  rows: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 2,
+    backgroundColor: "white",
+    paddingLeft: 2,
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    height: 30,
+    width: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    // borderWidth: 1,
+    // borderStyle: "solid",
+    // borderColor: "black",
+  },
+  margin: {
+    margin: 10,
+  },
+});
--- screens/SettingsPage.jsx
+++ screens/SettingsPage.jsx
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View, TouchableOpacity, ToastAndroid } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import React from "react";
 import { Button } from "@react-native-material/core";
 import { auth } from "../config/firebase";
@@ -6,7 +12,7 @@ import { signOut } from "firebase/auth";
 import { useContext } from "react";
 import { UserContext } from "../contexts/userContext";
 import { patchUserPreferences } from "../utils/api.js";
-import EditTagFrequency from "../components/EditTagFrequency";
+import DeveloperSettings from "../components/EditTagFrequency";
 
 const SettingsPage = () => {
   const { user } = useContext(UserContext);
@@ -21,20 +27,19 @@ const SettingsPage = () => {
       });
   };
 
-    const resetRecommendations = async () => {
-        try {
-            const data = '{"title":{"cat":1,"dog":1,"rekive":1,"techno":1,"aloxe":1,"ess":1,"t-shirt":1,"sage":1,"green":1,"reclaimed":1,"vintage":1,"unisex":1,"stone":1,"active":1,"boxer":1,"shorts":1,"polo":1,"ralph":1,"lauren":1,"icon":1,"logo":1,"heavyweight":1,"classic":1,"fit":1,"white":1},"color":{"red":1,"green":1,"stone":1,"white":1},"category":{"shirt":1,"activewear":1},"brand":{"asos":1,"adidas Originals":1,"Reclaimed Vintage":1,"Polo Ralph Lauren":1}}'
-            await patchUserPreferences(user, {preferences: data});
-            ToastAndroid.show('Reset Successful', ToastAndroid.SHORT);
-
-        } catch (err) {
-            console.log(err);
-        }
-     };
+  const resetRecommendations = async () => {
+    try {
+      const data =
+        '{"title":{"cat":1,"dog":1,"rekive":1,"techno":1,"aloxe":1,"ess":1,"t-shirt":1,"sage":1,"green":1,"reclaimed":1,"vintage":1,"unisex":1,"stone":1,"active":1,"boxer":1,"shorts":1,"polo":1,"ralph":1,"lauren":1,"icon":1,"logo":1,"heavyweight":1,"classic":1,"fit":1,"white":1},"color":{"red":1,"green":1,"stone":1,"white":1},"category":{"shirt":1,"activewear":1},"brand":{"asos":1,"adidas Originals":1,"Reclaimed Vintage":1,"Polo Ralph Lauren":1}}';
+      await patchUserPreferences(user, { preferences: data });
+      ToastAndroid.show("Reset Successful", ToastAndroid.SHORT);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <View style={styles.buttons}>
-      {/* <EditTagFrequency /> */}
       <Button
         onPress={resetRecommendations}
         title="Reset Recommendations?"
@@ -45,6 +50,7 @@ const SettingsPage = () => {
         title="Sign Out"
         style={styles.signout}
       ></Button>
+      <DeveloperSettings />
     </View>
   );
 };
